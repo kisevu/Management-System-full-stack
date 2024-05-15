1fc98c3e3f56ec11d128c11ffba9d148bc5ebf6a
ameda-kevin <amedakevin@gmail.com>
2024-05-16 01:25:15 +0300

    ui for the menus underway and designed it and also added the active status of the menus

--- my-ui/src/app/app-routing.module.ts
+++ my-ui/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ActivateAccountComponent } from './activate-account/activate-account.component';
+import { BookModule } from './modules/book/book.module';
 
 const routes: Routes = [
 {
@@ -16,6 +17,10 @@ const routes: Routes = [
 {
   path: 'activate-account',
   component: ActivateAccountComponent
+},
+{
+  path: 'books',
+  loadChildren: () => import ('./modules/book/book.module').then(m => m.BookModule)
 }
 ];
 
