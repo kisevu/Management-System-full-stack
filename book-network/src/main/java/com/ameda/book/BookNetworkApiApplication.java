e3967fe88848f69660095523d07a0a6204aac783
ameda-kevin <amedakevin@gmail.com>
2024-06-03 19:19:21 +0300

    docker compose update then integrate keycloak

--- book-network/src/main/java/com/ameda/book/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/ameda/book/BookNetworkApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -17,6 +18,18 @@ import java.time.LocalDateTime;
 public class BookNetworkApiApplication{
 	@Autowired
 	private RoleRepository roleRepository;
+
+	@Bean
+	public CommandLineRunner commandLineRunner (
+			RoleRepository roleRepository
+	){
+		return args ->{
+			var role = Role.builder()
+					.name("USER")
+					.build();
+			roleRepository.save(role);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookNetworkApiApplication.class, args);
