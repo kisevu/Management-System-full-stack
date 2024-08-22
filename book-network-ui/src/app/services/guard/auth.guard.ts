5a1c62f61155284659efa6cc794de735fa2f1fe6
ameda-kevin <amedakevin@gmail.com>
2024-08-22 17:17:44 +0300

    updated the interceptor and the guard to use the keycloak service rather than the usage of it which was on token service

--- book-network-ui/src/app/services/guard/auth.guard.ts
+++ book-network-ui/src/app/services/guard/auth.guard.ts
@@ -1,11 +1,11 @@
 import { CanActivateFn, Router } from '@angular/router';
 import {inject} from '@angular/core';
-import {TokenService} from '../token/token.service';
+import {KeycloakService} from '../keycloak/keycloak.service';
 
 export const authGuard: CanActivateFn = () => {
-  const tokenService: TokenService = inject(TokenService);
+  const keycloakService: KeycloakService = inject(KeycloakService);
   const router:Router = inject(Router);
-   if(tokenService.isTokenNotValid()){
+   if(keycloakService.keycloak?.isTokenExpired()){
       router.navigate(['login']);
       return false;
       }
--- book-network-ui/src/app/services/interceptor/http-interceptor.interceptor.ts
+++ book-network-ui/src/app/services/interceptor/http-interceptor.interceptor.ts
@@ -9,20 +9,21 @@ import {
 
 import { Observable } from 'rxjs';
 import {TokenService} from '../token/token.service';
+import {KeycloakService} from '../keycloak/keycloak.service';
 
 @Injectable()
 export class HttpInterceptorInterceptor implements HttpInterceptor {
 
   constructor(
-   private tokenService: TokenService
+   private keycloakService: KeycloakService
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-   const token = this.tokenService.token;
+   const token = this.keycloakService.keycloak.token;
      if(token){
       const authReq = request.clone({
        headers: new HttpHeaders({
-       Authorization: 'Bearer '+token
+       Authorization: `Bearer ${token}`
            })
          });
        return next.handle(authReq);
